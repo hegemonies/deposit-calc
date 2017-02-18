d4283e71b7baff47b477e90b8be8c5cfd86598f8
hegemonies <hegemonies@yandex.ru>
2017-02-18 12:21:15 +0700

    Request of input data

---
+++ deposit.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int sum_inset, time;
+	printf("Введите сумму вклада (не менее 10 000 рублей): ");
+	scanf("%d", &sum_inset);
+	printf("Введите срок вклада (не более 365 дней");
+	scanf("%d", &time);	
+
+	return 0;
+}
