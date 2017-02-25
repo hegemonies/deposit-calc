c40b6bf98e0e7fb26c4abb694f1684de4ee0ea16
hegemonies <hegemonies@yandex.ru>
2017-02-25 12:45:07 +0700

    Add .gitignore + fix for optimization + add calculation income of inset

--- deposit.c
+++ deposit.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#define min_arg_inset 10000
 
 int check_input(int sum, int time)
 {
-	if((sum > 10000) && ((time <= 365) && (time >= 0)))
+	if((sum > min_arg_inset) && ((time <= 365) && (time >= 0)))
 		return 0;
 	return 1;
 }
@@ -13,24 +14,29 @@ float calculation(int sum, int time)
 	if(time < 31)
 		tmp = sum * 0.9;
 	if((time > 30) && (time < 121)){
-		if(sum < 100001)
+		if(sum <= min_arg_inset)
 			tmp = sum * 1.02;
 		else
 			tmp = sum * 1.03;
 	}
 	if((time > 120) && (time < 241)){
-		if(sum < 100001)
+		if(sum <= min_arg_inset)
 			tmp = sum * 1.06;
 		else
 			tmp = sum * 1.08;
 	}
 	if(time > 240){
-		if(sum < 100001)
+		if(sum <= min_arg_inset)
 			tmp = sum * 1.12;
 		else
 			tmp = sum * 1.15;
 	}
 	return tmp;
+}
+
+int income(int first_sum, int last_sum)
+{
+	return (last_sum - first_sum);
 }
 
 int main()
@@ -47,6 +53,8 @@ int main()
 	} while(check_input(sum_inset, time) == 1);
 	
 	float calc = calculation(sum_inset, time);
+	int income_inset = income(sum_inset, calc);
 	printf("Годовой доход при сроке в %d дней и сумме %d рублей составит: %0.2f\n", time, sum_inset, calc);
+	printf("Доходность вклада составляет: %d\n", income_inset);
 	return 0;
 }
